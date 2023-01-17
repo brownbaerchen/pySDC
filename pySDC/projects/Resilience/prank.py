a512eaf0e24e349793cf58528168e5c518f5b5d5
Thomas Baumann <t.baumann@fz-juelich.de>
2023-01-17 15:38:08 +0100

    prank

---
+++ pySDC/projects/Resilience/prank.py
@@ -0,0 +1,1155 @@
+import pytest 
+a = 5
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+if a == 1:
+    print('prank')
+
+@pytest.mark.base
+def test_prank():
+    assert False
