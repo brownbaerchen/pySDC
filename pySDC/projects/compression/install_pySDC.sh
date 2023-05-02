74575623aa6ed7a1d63c770ec42b3bd4f22954d4
Thomas Baumann <t.baumann@fz-juelich.de>
2023-05-02 08:51:05 +0200

    63

--- pySDC/projects/compression/install_pySDC.sh
+++ pySDC/projects/compression/install_pySDC.sh
@@ -3,6 +3,9 @@
 # store current working directory to return here later
 current_dir=$(pwd)
 
+# load the spack environment variables
+source /opt/spack/share/spack/setup-env.sh
+
 # load libpressio in spack to make sure we are using the correct Python
 spack load libpressio
 
@@ -10,6 +13,7 @@ spack load libpressio
 cd /pySDC
 pip install -e .
 python -m pip install pytest
+python -m pip install coverage
 
 # go back to original working directory
 cd $current_dir
