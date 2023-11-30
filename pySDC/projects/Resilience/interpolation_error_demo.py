f71aea1fd3d251591df79d35de946aab5f02dd2e
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2023-11-30 10:56:11 +0100

    Removed everything from coverage report

--- pySDC/projects/Resilience/interpolation_error_demo.py
+++ pySDC/projects/Resilience/interpolation_error_demo.py
@@ -1,59 +1,4 @@
-def getStep(dt, num_nodes, quad_type, degree):
-    """
-    Get a step prepared for polynomial test equation
-
-    Args:
-        dt (float): Step size
-        num_nodes (int): Number of nodes
-        quad_type (str): Type of quadrature
-
-    Returns:
-       (dict): Stats object generated during the run
-       (pySDC.Controller.controller): Controller used in the run
-    """
-    from pySDC.implementations.problem_classes.polynomial_test_problem import polynomial_testequation
-    from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
-    from pySDC.implementations.convergence_controller_classes.estimate_polynomial_error import (
-        EstimatePolynomialError,
-    )
-
-    from pySDC.implementations.sweeper_classes.generic_implicit import generic_implicit as sweeper_class
-
-    # initialize level parameters
-    level_params = {}
-    level_params['dt'] = dt
-    level_params['restol'] = 1.0
-
-    # initialize sweeper parameters
-    sweeper_params = {}
-    sweeper_params['quad_type'] = quad_type
-    sweeper_params['num_nodes'] = num_nodes
-
-    problem_params = {'degree': degree}
-
-    # initialize step parameters
-    step_params = {}
-    step_params['maxiter'] = 0
-
-    # initialize controller parameters
-    controller_params = {}
-    controller_params['logger_level'] = 30
-    controller_params['mssdc_jac'] = False
-
-    # fill description dictionary for easy step instantiation
-    description = {}
-    description['problem_class'] = polynomial_testequation
-    description['problem_params'] = problem_params
-    description['sweeper_class'] = sweeper_class
-    description['sweeper_params'] = sweeper_params
-    description['level_params'] = level_params
-    description['step_params'] = step_params
-    description['convergence_controllers'] = {EstimatePolynomialError: {}}
-
-    return controller_nonMPI(num_procs=1, controller_params=controller_params, description=description).MS[0]
-
-
-def plot_interpolation_error(**kwargs):
+def plot_interpolation_error(**kwargs):  # pragma: no cover
     import numpy as np
     import matplotlib.pyplot as plt
     from pySDC.helpers.plot_helper import setup_mpl, figsize_by_journal
@@ -63,8 +8,62 @@ def plot_interpolation_error(**kwargs):
     setup_mpl()
     from pySDC.projects.Resilience.collocation_adaptivity import CMAP
 
+    def getStep(dt, num_nodes, quad_type, degree):
+        """
+        Get a step prepared for polynomial test equation
+
+        Args:
+            dt (float): Step size
+            num_nodes (int): Number of nodes
+            quad_type (str): Type of quadrature
+
+        Returns:
+           (dict): Stats object generated during the run
+           (pySDC.Controller.controller): Controller used in the run
+        """
+        from pySDC.implementations.problem_classes.polynomial_test_problem import polynomial_testequation
+        from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
+        from pySDC.implementations.convergence_controller_classes.estimate_polynomial_error import (
+            EstimatePolynomialError,
+        )
+
+        from pySDC.implementations.sweeper_classes.generic_implicit import generic_implicit as sweeper_class
+
+        # initialize level parameters
+        level_params = {}
+        level_params['dt'] = dt
+        level_params['restol'] = 1.0
+
+        # initialize sweeper parameters
+        sweeper_params = {}
+        sweeper_params['quad_type'] = quad_type
+        sweeper_params['num_nodes'] = num_nodes
+
+        problem_params = {'degree': degree}
+
+        # initialize step parameters
+        step_params = {}
+        step_params['maxiter'] = 0
+
+        # initialize controller parameters
+        controller_params = {}
+        controller_params['logger_level'] = 30
+        controller_params['mssdc_jac'] = False
+
+        # fill description dictionary for easy step instantiation
+        description = {}
+        description['problem_class'] = polynomial_testequation
+        description['problem_params'] = problem_params
+        description['sweeper_class'] = sweeper_class
+        description['sweeper_params'] = sweeper_params
+        description['level_params'] = level_params
+        description['step_params'] = step_params
+        description['convergence_controllers'] = {EstimatePolynomialError: {}}
+
+        return controller_nonMPI(num_procs=1, controller_params=controller_params, description=description).MS[0]
+
     # prepare figure
-    fig, ax = plt.subplots(figsize=figsize_by_journal('JSC_beamer', 1.0, 0.7))
+    fig, ax = plt.subplots(figsize=figsize_by_journal('JSC_beamer', 0.5, 0.75))
 
     args = {
         'num_nodes': 3,
@@ -130,7 +129,8 @@ def plot_interpolation_error(**kwargs):
 
     ax.legend(frameon=False)
     ax.set_xlabel(r'$t$')
-    plt.show()
+    fig.tight_layout()
+    fig.savefig('data/paper/polynomial_interpolation_error.pdf')
 
 
 if __name__ == "__main__":
