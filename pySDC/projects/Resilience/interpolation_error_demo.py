3be8fe64e46565d786609a662ce6c835c055d642
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2023-12-01 15:14:50 +0100

    work in progress

--- pySDC/projects/Resilience/interpolation_error_demo.py
+++ pySDC/projects/Resilience/interpolation_error_demo.py
@@ -1,3 +1,164 @@
+from pySDC.core.Hooks import hooks
+
+
+class LogSolutionAllNodes(hooks):
+    def post_iteration(self, step, level_number):
+        super().post_iteration(step, level_number)
+
+        L = step.levels[level_number]
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type='u',
+            value=L.u.copy(),
+        )
+
+
+def getController(dt, num_nodes, quad_type, degree, prob='poly'):
+    """
+    Get a step prepared for polynomial test equation
+
+    Args:
+        dt (float): Step size
+        num_nodes (int): Number of nodes
+        quad_type (str): Type of quadrature
+
+    Returns:
+       (dict): Stats object generated during the run
+       (pySDC.Controller.controller): Controller used in the run
+    """
+    from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
+    from pySDC.implementations.convergence_controller_classes.estimate_polynomial_error import (
+        EstimatePolynomialError,
+    )
+
+    if prob == 'poly':
+        from pySDC.implementations.problem_classes.polynomial_test_problem import (
+            polynomial_testequation as problem_class,
+        )
+    elif prob == 'vdp':
+        from pySDC.implementations.problem_classes.Van_der_Pol_implicit import vanderpol as problem_class
+
+    from pySDC.implementations.sweeper_classes.generic_implicit import generic_implicit as sweeper_class
+
+    # initialize level parameters
+    level_params = {}
+    level_params['dt'] = dt
+    level_params['restol'] = -1
+
+    # initialize sweeper parameters
+    sweeper_params = {}
+    sweeper_params['quad_type'] = quad_type
+    sweeper_params['num_nodes'] = num_nodes
+    sweeper_params['initial_guess'] = 'zero'
+
+    problem_params = {}
+    if prob == 'poly':
+        problem_params['degree'] = degree
+    elif prob == 'vdp':
+        problem_params['mu'] = 0.0
+
+    # initialize step parameters
+    step_params = {}
+    step_params['maxiter'] = 5
+
+    # initialize controller parameters
+    controller_params = {}
+    controller_params['logger_level'] = 30
+    controller_params['mssdc_jac'] = False
+    controller_params['hook_class'] = LogSolutionAllNodes
+
+    # fill description dictionary for easy step instantiation
+    description = {}
+    description['problem_class'] = problem_class
+    description['problem_params'] = problem_params
+    description['sweeper_class'] = sweeper_class
+    description['sweeper_params'] = sweeper_params
+    description['level_params'] = level_params
+    description['step_params'] = step_params
+    description['convergence_controllers'] = {EstimatePolynomialError: {}}
+
+    return controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+
+
+def plot_embedded_error(**kwargs):  # pragma: no cover
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from pySDC.helpers.plot_helper import setup_mpl, figsize_by_journal
+    from pySDC.core.Lagrange import LagrangeApproximation
+    from pySDC.core.Collocation import CollBase
+    from pySDC.helpers.stats_helper import get_sorted
+
+    setup_mpl()
+    from pySDC.projects.Resilience.collocation_adaptivity import CMAP
+
+    # prepare figure
+    fig, ax = plt.subplots(figsize=figsize_by_journal('JSC_beamer', 0.5, 0.75))
+
+    args = {
+        'num_nodes': 3,
+        'quad_type': 'RADAU-RIGHT',
+        'dt': 5.0,
+        'degree': 6,
+        'prob': 'vdp',
+        **kwargs,
+    }
+
+    # prepare variables
+    controller = getController(**args)
+    step = controller.MS[0]
+    level = step.levels[0]
+    prob = level.prob
+    nodes = np.append([0], level.sweep.coll.nodes) * args['dt']
+
+    u_0 = prob.u_exact(t=0)
+    _, stats = controller.run(u0=u_0, t0=0, Tend=args['dt'])
+
+    u = get_sorted(stats, sortby='iter', type='u')
+
+    # initialize variables
+    M = len(level.u)
+    interpolate_on_node = (
+        M - 2
+    )  # M = coll.num_nodes + 1, so this is shifted wrt to the implementation in the convergence controller
+
+    # plot exact solution
+    _coll = CollBase(100, 0, args['dt'], quad_type='LOBATTO')
+    t = _coll.nodes
+    u_exact = [prob.u_exact(me) for me in t]
+    ax.plot(t, [me[0] for me in u_exact], label=f'Exact solution', color=CMAP[0])
+
+    # plot SDC interpolated polynomial
+    for k, ls in zip([4, 5], ['-.', '--']):
+        u_k = np.array(u[k - 1][1]).T[0]
+        interpolator = LagrangeApproximation(points=nodes)
+        interpolation_matrix = interpolator.getInterpolationMatrix(t)
+        u_SDC = interpolation_matrix @ u_k
+        ax.plot(t, u_SDC, label=rf'Degree {k} SDC approximation', color=CMAP[k], ls=ls)
+        ax.scatter(nodes, u_k, color=CMAP[k])
+
+    # # plot error
+    # u_reduced_at_node = u_reduced[np.argmin((t - nodes[interpolate_on_node]) ** 2)]
+    # ax.plot(
+    #     [nodes[interpolate_on_node]] * 2,
+    #     [u[interpolate_on_node], u_reduced_at_node],
+    #     color='black',
+    #     marker='x',
+    #     label=f'Order {len(nodes[reduced_points])} error estimate',
+    #     ls=':',
+    # )
+
+    ax.legend(frameon=False)
+    ax.set_xlabel(r'$t$')
+    fig.tight_layout()
+    fig.savefig('data/paper/embedded_error.pdf')
+    plt.show()
+
+
 def plot_interpolation_error(**kwargs):  # pragma: no cover
     import numpy as np
     import matplotlib.pyplot as plt
@@ -7,60 +168,6 @@ def plot_interpolation_error(**kwargs):  # pragma: no cover
 
     setup_mpl()
     from pySDC.projects.Resilience.collocation_adaptivity import CMAP
-
-    def getStep(dt, num_nodes, quad_type, degree):
-        """
-        Get a step prepared for polynomial test equation
-
-        Args:
-            dt (float): Step size
-            num_nodes (int): Number of nodes
-            quad_type (str): Type of quadrature
-
-        Returns:
-           (dict): Stats object generated during the run
-           (pySDC.Controller.controller): Controller used in the run
-        """
-        from pySDC.implementations.problem_classes.polynomial_test_problem import polynomial_testequation
-        from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
-        from pySDC.implementations.convergence_controller_classes.estimate_polynomial_error import (
-            EstimatePolynomialError,
-        )
-
-        from pySDC.implementations.sweeper_classes.generic_implicit import generic_implicit as sweeper_class
-
-        # initialize level parameters
-        level_params = {}
-        level_params['dt'] = dt
-        level_params['restol'] = 1.0
-
-        # initialize sweeper parameters
-        sweeper_params = {}
-        sweeper_params['quad_type'] = quad_type
-        sweeper_params['num_nodes'] = num_nodes
-
-        problem_params = {'degree': degree}
-
-        # initialize step parameters
-        step_params = {}
-        step_params['maxiter'] = 0
-
-        # initialize controller parameters
-        controller_params = {}
-        controller_params['logger_level'] = 30
-        controller_params['mssdc_jac'] = False
-
-        # fill description dictionary for easy step instantiation
-        description = {}
-        description['problem_class'] = polynomial_testequation
-        description['problem_params'] = problem_params
-        description['sweeper_class'] = sweeper_class
-        description['sweeper_params'] = sweeper_params
-        description['level_params'] = level_params
-        description['step_params'] = step_params
-        description['convergence_controllers'] = {EstimatePolynomialError: {}}
-
-        return controller_nonMPI(num_procs=1, controller_params=controller_params, description=description).MS[0]
 
     # prepare figure
     fig, ax = plt.subplots(figsize=figsize_by_journal('JSC_beamer', 0.5, 0.75))
@@ -74,7 +181,7 @@ def plot_interpolation_error(**kwargs):  # pragma: no cover
     }
 
     # prepare variables
-    step = getStep(**args)
+    step = getController(**args).MS[0]
     level = step.levels[0]
     prob = level.prob
     nodes = np.append([0], level.sweep.coll.nodes)
@@ -99,13 +206,13 @@ def plot_interpolation_error(**kwargs):  # pragma: no cover
     # plot exact solution
     _coll = CollBase(100, 0, args['dt'], quad_type='LOBATTO')
     t = _coll.nodes
-    ax.plot(t, [prob.u_exact(me) for me in t], label=f'Degree {args["degree"]} exact solution', color=CMAP[0])
+    ax.plot(t, [prob.u_exact(me) for me in t], label=f'Degree {args["degree"]} polynomial', color=CMAP[0])
 
     # plot SDC interpolated polynomial
     interpolator = LagrangeApproximation(points=nodes)
     interpolation_matrix = interpolator.getInterpolationMatrix(t)
     u_SDC = interpolation_matrix @ u
-    ax.plot(t, u_SDC, label=rf'Degree {level.sweep.coll.order} SDC solution', color=CMAP[1], ls='--')
+    ax.plot(t, u_SDC, label=rf'Degree {level.sweep.coll.order} SDC approximation', color=CMAP[1], ls='--')
     ax.scatter(nodes, u, color=CMAP[1])
 
     # plot secondary interpolation
@@ -134,4 +241,5 @@ def plot_interpolation_error(**kwargs):  # pragma: no cover
 
 
 if __name__ == "__main__":
-    plot_interpolation_error(num_nodes=3, degree=6)
+    plot_embedded_error()
+    # plot_interpolation_error(num_nodes=3, degree=6)
