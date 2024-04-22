61a8e2ece148a2d995b5995c61eecc17174322ef
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2024-04-22 13:39:15 +0200

    Added plotting

--- pySDC/projects/GPU/measure_cable_speed.py
+++ pySDC/projects/GPU/measure_cable_speed.py
@@ -1,8 +1,15 @@
-import cupy as xp
-from cupy.cuda import nccl
-from cupyx.profiler import benchmark
 from mpi4py import MPI
 import pickle
+
+
+powers = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+shapes = [
+    (
+        2**power,
+        2**3,
+    )
+    for power in powers
+]
 
 
 def get_path(name, shape):
@@ -10,6 +17,8 @@ def get_path(name, shape):
 
 
 def get_communicators():
+    from cupy.cuda import nccl
+
     comm = MPI.COMM_WORLD
     uid = comm.bcast(nccl.get_unique_id(), root=0)
     commNCCL = nccl.NcclCommunicator(comm.size, uid, comm.rank)
@@ -17,6 +26,10 @@ def get_communicators():
 
 
 def measure_speed(comm, commNCCL, shape, name='', **kwargs):
+    import cupy as xp
+    from cupy.cuda import nccl
+    from cupyx.profiler import benchmark
+
     data = xp.random.random(shape, dtype='d')
     size = data.nbytes
     results = {}
@@ -41,7 +54,13 @@ def measure_speed(comm, commNCCL, shape, name='', **kwargs):
             data[:] = 0
 
         res = benchmark(func, (data,), **kwargs)
-        results[mode] = {'CPU': res.cpu_times.mean(), 'GPU': res.gpu_times.mean(), 'size': size}
+        results[mode] = {
+            'CPU_time': res.cpu_times.mean(),
+            'CPU_std': res.cpu_times.std,
+            'GPU_time': res.gpu_times.mean(),
+            'GPU_std': res.gpu_times.std,
+            'size': size,
+        }
         assert xp.linalg.norm(data, 2.0) > 0
 
     print(results)
@@ -50,15 +69,27 @@ def measure_speed(comm, commNCCL, shape, name='', **kwargs):
         pickle.dump(results, file)
 
 
-if __name__ == '__main__':
+def record(shapes):
     comm, commNCCL = get_communicators()
-    for power in [22, 24, 26]:
-        measure_speed(
-            comm,
-            commNCCL,
-            (
-                2**power,
-                2**3,
-            ),
-            n_repeat=1000,
-        )
+    for shape in shapes:
+        measure_speed(comm, commNCCL, shape, n_repeat=1000)
+
+
+def plot(shapes, name=''):
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+
+    results = []
+    for shape in shapes:
+        with open(get_path(name=name, shape=shape), 'rb') as file:
+            data = pickle.load(file)
+        results += [data]
+
+    for lib in ['NCCL', 'MPI']:
+        ax.loglog([me[lib]['size'] for me in results], [me[lib]['GPU'] for me in results], label=lib)
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot(shapes)
