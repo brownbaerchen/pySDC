d89c5a70b9146fc8e9049c2a2c7bf0d8bd4ada05
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2024-07-28 17:56:36 +0200

    Added file

---
+++ pySDC/projects/GPU/RayleighBenard.py
@@ -0,0 +1,177 @@
+from mpi4py import MPI
+import numpy as np
+from pySDC.implementations.hooks.log_solution import LogToFile
+
+from pySDC.core.convergence_controller import ConvergenceController
+
+
+class CFLLimit(ConvergenceController):
+    def get_new_step_size(self, controller, step, **kwargs):
+        max_step_size = np.inf
+        min_step_size = 1e-3
+
+        L = step.levels[0]
+        P = step.levels[0].prob
+
+        cfl = 0.4
+
+        grid_spacing_x = P.X[1:, :] - P.X[:-1, :]
+        grid_spacing_z = P.Z[:, :-1] - P.Z[:, 1:]
+
+        iu, iv = P.index(['u', 'v'])
+        for u in step.levels[0].u:
+            max_step_size = min([max_step_size, P.xp.min(grid_spacing_x / abs(u[iu][:-1, :]))])
+            max_step_size = min([max_step_size, P.xp.min(grid_spacing_z / abs(u[iv][:, :-1]))])
+
+        dt_new = L.status.dt_new if L.status.dt_new else L.params.dt
+        L.status.dt_new = min([dt_new, max_step_size])
+        L.status.dt_new = max([min_step_size, L.status.dt_new])
+        self.log(f'dt max: {max_step_size:.2e} -> New step size: {L.status.dt_new:.2e}', step)
+
+
+def run_RBC():
+    from pySDC.implementations.problem_classes.RayleighBenard import RayleighBenard
+    from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
+    from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
+    from pySDC.implementations.convergence_controller_classes.adaptivity import Adaptivity
+    from pySDC.implementations.problem_classes.generic_spectral import compute_residual_DAE
+
+    from pySDC.implementations.hooks.live_plotting import PlotPostStep
+    from pySDC.projects.GPU.hooks.LogGrid import LogGrid
+
+    imex_1st_order.compute_residual = compute_residual_DAE
+
+    comm = MPI.COMM_WORLD
+    LogToFile.path = './data/'
+    LogToFile.file_name = f'RBC-{comm.rank}'
+    LogToFile.process_solution = lambda L: {
+        't': L.time + L.dt,
+        'u': L.uend.view(np.ndarray),
+        'vorticity': L.prob.compute_vorticity(L.uend).view(np.ndarray),
+    }
+    LogGrid.file_name = f'RBC-grid-{comm.rank}'
+    LogGrid.file_logger = LogToFile
+
+    level_params = {}
+    level_params['dt'] = 0.25
+    level_params['restol'] = 1e-6
+
+    convergence_controllers = {
+        # Adaptivity: {'e_tol': 1e0}
+        CFLLimit: {}
+    }
+
+    sweeper_params = {}
+    sweeper_params['quad_type'] = 'RADAU-RIGHT'
+    sweeper_params['num_nodes'] = 1
+    sweeper_params['QI'] = 'IE'
+    sweeper_params['QE'] = 'EE'
+
+    problem_params = {
+        'comm': comm,
+        'nx': 2**9,
+        'nz': 2**9,
+    }
+
+    step_params = {}
+    step_params['maxiter'] = 1
+
+    controller_params = {}
+    controller_params['logger_level'] = 15 if comm.rank == 0 else 40
+    controller_params['hook_class'] = [LogToFile, LogGrid]
+    controller_params['mssdc_jac'] = False
+
+    description = {}
+    description['problem_class'] = RayleighBenard
+    description['problem_params'] = problem_params
+    description['sweeper_class'] = imex_1st_order
+    description['sweeper_params'] = sweeper_params
+    description['level_params'] = level_params
+    description['step_params'] = step_params
+    description['convergence_controllers'] = convergence_controllers
+
+    controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+
+    t0 = 0.0
+    Tend = 500
+    P = controller.MS[0].levels[0].prob
+    uinit = P.u_exact(t0)
+
+    uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
+
+    return stats
+
+
+def plot_RBC(size, quantitiy='T'):
+    import matplotlib.pyplot as plt
+    from pySDC.implementations.hooks.log_solution import LogToFile
+    from pySDC.projects.GPU.hooks.LogGrid import LogGrid
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    from pySDC.implementations.problem_classes.RayleighBenard import RayleighBenard
+
+    P = RayleighBenard()
+
+    LogToFile.path = './data/'
+    LogGrid.file_logger = LogToFile
+
+    fig = P.get_fig()
+    cax = P.cax
+    axs = fig.get_axes()
+
+    for i in range(400):
+        buffer = {}
+        vmin = {quantitiy: 0, 'vorticity': 0}
+        vmax = {quantitiy: 0, 'vorticity': 0}
+        for rank in range(size):
+            LogToFile.file_name = f'RBC-{rank}'
+            LogGrid.file_name = f'RBC-grid-{rank}'
+
+            buffer[f'u-{rank}'] = LogToFile.load(i)
+            buffer[f'Z-{rank}'], buffer[f'X-{rank}'] = LogGrid.load()
+
+            vmin['vorticity'] = min([vmin['vorticity'], buffer[f'u-{rank}']['vorticity'].real.min()])
+            vmax['vorticity'] = max([vmax['vorticity'], abs(buffer[f'u-{rank}']['vorticity'].real).max()])
+            vmin[quantitiy] = min([vmin[quantitiy], buffer[f'u-{rank}']['u'][P.index(quantitiy)].real.min()])
+            vmax[quantitiy] = max([vmax[quantitiy], buffer[f'u-{rank}']['u'][P.index(quantitiy)].real.max()])
+
+        for rank in range(size):
+            im = axs[1].pcolormesh(
+                buffer[f'X-{rank}'],
+                buffer[f'Z-{rank}'],
+                buffer[f'u-{rank}']['vorticity'].real,
+                vmin=-vmax['vorticity'],
+                vmax=vmax['vorticity'],
+                cmap='bwr',
+            )
+            fig.colorbar(im, cax[1])
+            im = axs[0].pcolormesh(
+                buffer[f'X-{rank}'],
+                buffer[f'Z-{rank}'],
+                buffer[f'u-{rank}']['u'][P.index(quantitiy)].real,
+                vmin=-vmax[quantitiy],
+                vmax=vmax[quantitiy],
+                cmap='bwr',
+            )
+            fig.colorbar(im, cax[0])
+            axs[0].set_title(f't={buffer[f"u-{rank}"]["t"]:.2e}')
+            axs[1].set_xlabel('x')
+            axs[1].set_ylabel('z')
+        plt.pause(1e-9)
+        fig.savefig(f'simulation_plots/RBC{i:06d}.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run', type=bool)
+    parser.add_argument('--plot', type=bool)
+
+    args = parser.parse_args()
+
+    if args.run:
+        run_RBC()
+    if MPI.COMM_WORLD.rank == 0 and args.plot:
+        plot_RBC(MPI.COMM_WORLD.size)
