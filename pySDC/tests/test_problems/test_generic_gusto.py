4352e6dc2c07e2d99574a416078ef0a2bb1eaa75
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2025-01-14 09:53:35 +0000

    Comparing forward and backward Euler in pySDC to gusto reference now. Transport methods not yet working!

--- pySDC/tests/test_problems/test_generic_gusto.py
+++ pySDC/tests/test_problems/test_generic_gusto.py
@@ -2,7 +2,8 @@ import pytest
 
 
 @pytest.mark.firedrake
-def test_generic_gusto():
+@pytest.mark.parametrize('use_transport_scheme', [True, False])
+def test_generic_gusto(use_transport_scheme):
     """
     Use Williamson test Case 5 (flow over a mountain) of Williamson et al, 1992:
     ``A standard test set for numerical approximations to the shallow water
@@ -11,6 +12,7 @@ def test_generic_gusto():
     The example here uses the icosahedral sphere mesh and degree 1 spaces.
     """
     import firedrake as fd
+    import numpy as np
     from pySDC.implementations.problem_classes.GenericGusto import GenericGusto, setup_equation
     from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
 
@@ -32,6 +34,8 @@ def test_generic_gusto():
         RelativeVorticity,
         RungeKuttaFormulation,
         SubcyclingOptions,
+        ThetaMethod,
+        Timestepper,
     )
 
     # ------------------------------------------------------------------------ #
@@ -76,20 +80,14 @@ def test_generic_gusto():
     tpexpr = mountain_height * (1 - r / R0)
     eqns = ShallowWaterEquations(domain, parameters, fexpr=fexpr, topog_expr=tpexpr)
 
-    # Transport schemes
-    # subcycling_options = SubcyclingOptions(subcycle_by_courant=0.25)
-    # transported_fields = [
-    #     SSPRK3(domain, "u", subcycling_options=subcycling_options),
-    #     SSPRK3(
-    #         domain, "D", subcycling_options=subcycling_options,
-    #         rk_formulation=RungeKuttaFormulation.linear
-    #     )
-    # ]
     transport_methods = [DGUpwind(eqns, "u"), DGUpwind(eqns, "D", advective_then_flux=True)]
+    spatial_methods = None
 
-    eqns_with_transport = setup_equation(eqns, transport_methods)
+    if use_transport_scheme:
+        eqns = setup_equation(eqns, transport_methods)
+        spatial_methods = transport_methods
 
-    problem = GenericGusto(eqns_with_transport)
+    problem = GenericGusto(eqns)
 
     # ------------------------------------------------------------------------ #
     # Initial conditions
@@ -105,6 +103,30 @@ def test_generic_gusto():
     D0.interpolate(Dexpr)
 
     # ------------------------------------------------------------------------ #
+    # Prepare gusto reference methods
+    # ------------------------------------------------------------------------ #
+
+    # TODO: Can I get rid of the output here?
+
+    output = OutputParameters(
+        dirname=f'./tmp{np.random.randint(1e9)}', dumplist_latlon=['D'], dumpfreq=1000000000,
+        dump_vtus=True, dump_nc=False, dumplist=['D', 'topography']
+    )
+    diagnostic_fields = [Sum('D', 'topography'), RelativeVorticity(),
+                         MeridionalComponent('u'), ZonalComponent('u')]
+    io = IO(domain, output, diagnostic_fields=diagnostic_fields)
+    stepper_backward = Timestepper(eqns, ThetaMethod(domain, theta=1.), io, spatial_methods=spatial_methods)
+
+    output = OutputParameters(
+        dirname=f'./tmp{np.random.randint(1e9)}', dumplist_latlon=['D'], dumpfreq=1000000000,
+        dump_vtus=True, dump_nc=False, dumplist=['D', 'topography']
+    )
+    diagnostic_fields = [Sum('D', 'topography'), RelativeVorticity(),
+                         MeridionalComponent('u'), ZonalComponent('u')]
+    io = IO(domain, output, diagnostic_fields=diagnostic_fields)
+    stepper_forward = Timestepper(eqns, ThetaMethod(domain, theta=0.), io, spatial_methods=spatial_methods)
+
+    # ------------------------------------------------------------------------ #
     # Run tests
     # ------------------------------------------------------------------------ #
 
@@ -117,7 +139,35 @@ def test_generic_gusto():
     test_error = abs(u_start - un) / abs(u_start)
 
     assert error < 5e-2 * test_error
+    print(error)
+
+    # test forward Euler step
+    stepper_forward.fields("u").assign(u0)
+    stepper_forward.fields("D").assign(D0)
+    stepper_forward.run(t=0, tmax=dt)
+    un_ref = problem.dtype_u(problem.init)
+    u, D = un_ref.subfunctions[:]
+    u.assign(stepper_forward.fields('u'))
+    D.assign(stepper_forward.fields('D'))
+    un_forward = u_start + dt * problem.eval_f(u_start)
+    error = abs(un_forward - un_ref) / abs(un_ref)
+    print(error, test_error)
+
+    assert error < np.finfo(float).eps * 1e2, f'Forward Euler does not match reference implementation! Got relative difference of {error}'
+
+    # test backward Euler step
+    stepper_backward.fields("u").assign(u0)
+    stepper_backward.fields("D").assign(D0)
+    stepper_backward.run(t=0, tmax=dt)
+    un_ref = problem.dtype_u(problem.init)
+    u, D = un_ref.subfunctions[:]
+    u.assign(stepper_backward.fields('u'))
+    D.assign(stepper_backward.fields('D'))
+    error = abs(un - un_ref) / abs(un_ref)
+    print(error, test_error)
+
+    assert error < np.finfo(float).eps * 1e2, f'Backward Euler does not match reference implementation! Got relative difference of {error}'
 
 
 if __name__ == '__main__':
-    test_generic_gusto()
+    test_generic_gusto(False)
