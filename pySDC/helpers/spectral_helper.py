e23e07ac0497537e32f9fd31981a265a4faa0cf1
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2025-06-17 11:38:29 +0200

    Added a comment

--- pySDC/helpers/spectral_helper.py
+++ pySDC/helpers/spectral_helper.py
@@ -1561,6 +1561,10 @@ class SpectralHelper:
         self.BC_rhs_mask = self.newDistArray().astype(bool)
 
     def newDistArray(self, pfft=None, forward_output=True, val=0, rank=1, view=False):
+        """
+        Get an empty distributed array. This is almost a copy of the function of the same name from mpi4py-fft, but
+        takes care of all the solution components in the tensor.
+        """
         if self.comm is None:
             return self.xp.zeros(self.init[0], dtype=self.init[2])
         from mpi4py_fft.distarray import DistArray
