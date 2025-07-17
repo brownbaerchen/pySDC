f503125c3683b8bb9bce0b545fcd7936e50dbd75
Thomas <t.baumann@fz-juelich.de>
2025-07-17 11:35:40 +0200

    Refined measurement script
    #!!!!!! WARNING: FLAKEHEAVEN FAILED !!!!!!: 
    
    #:

--- pySDC/projects/GPU/measure_spgemm_speed.py
+++ pySDC/projects/GPU/measure_spgemm_speed.py
@@ -17,9 +17,9 @@ def get_mats(N, useGPU, types):
 
     for i in range(len(mats)):
         if types[i] == 'csc':
-            mats[i] = mats[i].tocsc()
+            mats[i] = sp.csc_matrix(mats[i])
         elif types[i] == 'csr':
-            mats[i] = mats[i].tocsr()
+            mats[i] = sp.csr_matrix(mats[i])
         else:
             raise NotImplementedError
 
@@ -27,14 +27,24 @@ def get_mats(N, useGPU, types):
 
 
 def single_experiment(mats, useGPU):
-    t0 = perf_counter()
-    mats[0] @ mats[1]
     if useGPU:
         import cupy as cp
 
-        cp.cuda.get_current_stream().synchronize()
-    t1 = perf_counter()
-    return t1 - t0
+        t0 = cp.cuda.Event()
+        t1 = cp.cuda.Event()
+        t0.record()
+    else:
+        t0 = perf_counter()
+
+    mats[0] @ mats[1]
+
+    if useGPU:
+        t1.record()
+        t1.synchronize()
+        return cp.cuda.get_elapsed_time(t0, t1) / 1e3
+    else:
+        t1 = perf_counter()
+        return t1 - t0
 
 
 def sample_experiment(mats, useGPU, num_experiments=20):
@@ -48,9 +58,10 @@ def get_path(N, useGPU, types):
     return f'./data/spgemm_timings_N{N}_useGPU{useGPU}_{types[0]}x{types[1]}.pickle'
 
 
-def get_label(useGPU, types):
+def get_label(useGPU, types=None):
     label = 'GPU' if useGPU else 'CPU'
-    label += f' {types[0].upper()}x{types[1].upper()}'
+    if types is not None:
+        label += f' {types[0].upper()}x{types[1].upper()}'
     return label
 
 
@@ -58,11 +69,12 @@ def record_experiment(N, useGPU, types, **kwargs):
     mats = get_mats(N, useGPU, types)
     path = get_path(N, useGPU, types)
 
+    _ = sample_experiment(mats, useGPU, num_experiments=2)
     timings = sample_experiment(mats, useGPU, **kwargs)
 
     with open(path, 'wb') as file:
         pickle.dump(timings, file)
-    print(f'Recorded timings at {path}')
+    print(f'Recorded timings at {path}', flush=True)
 
 
 def record_experiments(Ns, **kwargs):
@@ -84,17 +96,37 @@ def plot_timings(Ns, ax, **kwargs):
 
 
 def compare_csr_csc(useGPU=False, record=False):
-    Ns = range(2, 64, 2)
+    Ns = [2**me for me in range(1, 8)]
+    Ns = range(16, 100, 4)
     types = [['csr', 'csc'], ['csc', 'csc'], ['csc', 'csr'], ['csr', 'csr']]
-    useGPU = False
+    types = [['csr', 'csr'], ['csc', 'csc']]
     fig, ax = plt.subplots()
 
     for _types in types:
         if record:
             record_experiments(Ns, useGPU=useGPU, types=_types)
         plot_timings(Ns, ax=ax, useGPU=useGPU, types=_types)
+    figpath = f'plots/SPGEMM_csr_vs_csc_{get_label(useGPU)}.pdf'
+    plt.savefig(figpath, bbox_inches='tight')
+    print(f'Plotted {figpath}', flush=True)
     plt.show()
 
 
+def compare_CPU_GPU(record=False):
+    types = [['csc', 'csc']]
+    Ns = range(16, 100, 4)
+    fig, ax = plt.subplots()
+    for useGPU in [True, False]:
+        for _types in types:
+            if record:
+                record_experiments(Ns, useGPU=useGPU, types=_types)
+            plot_timings(Ns, ax=ax, useGPU=useGPU, types=_types)
+    figpath = f'plots/SPGEMM_CPU_vs_GPU_{get_label(useGPU)}.pdf'
+    plt.savefig(figpath, bbox_inches='tight')
+    print(f'Plotted {figpath}', flush=True)
+
+
 if __name__ == '__main__':
-    compare_csr_csc(False, True)
+    # for useGPU in [True, False]:
+    #     compare_csr_csc(useGPU, record=False)
+    compare_CPU_GPU(False)
