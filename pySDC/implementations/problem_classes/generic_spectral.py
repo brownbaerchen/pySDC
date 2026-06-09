1ff311c5ca63c2318cbbd253becf222d829544b9
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-06-09 21:20:47 +0200

    Minor performance enhancement

--- pySDC/implementations/problem_classes/generic_spectral.py
+++ pySDC/implementations/problem_classes/generic_spectral.py
@@ -526,7 +526,7 @@ class GenericSpectralLinear(Problem):
         assert self.left_preconditioner
         self.logger.debug(f'Starting splitting of global matrix into {len(subproblem_masks)} sub-matrices')
         A = A.tolil()
-        sub_As = [(A[mask][:, mask]).tocsc() for mask in subproblem_masks]
+        sub_As = [(A[mask[:, None], mask]).tocsc() for mask in subproblem_masks]
         self.logger.debug(
             f'Split the global matrix of shape {A.shape} into {len(sub_As)} many matrices of shape {sub_As[0].shape}'
         )
